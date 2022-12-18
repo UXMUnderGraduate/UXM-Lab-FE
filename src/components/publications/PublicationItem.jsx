0951fb611eb4e37159cf175a2bdd8dd1c914ec14
MinsuKim <minsu020302@gmail.com>
2022-12-18 20:34:24 +0900

    Design : 반응형 추가

--- src/components/publications/PublicationItem.jsx
+++ src/components/publications/PublicationItem.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Publication = (props) => {
   return (
-    <li className='publications-list_item'>
-      <p className='font-medium text-base'>{'– ' + props.title}</p>
+    <li className='publication_item'>
+      <p className='font-medium text-sm sm:text-base'>{'– ' + props.title}</p>
     </li>
   );
 };
