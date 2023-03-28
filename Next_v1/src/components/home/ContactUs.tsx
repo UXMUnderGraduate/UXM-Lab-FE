660fa7f2c2fe7e1cf94ce94bd164c4f22893aed9
MinsuKim <minsu020302@gmail.com>
2023-03-28 11:32:32 +0900

    Design : ContactUs컴포넌트 css변경 및 a태그 href mailto추가

--- Next_v1/src/components/home/ContactUs.tsx
+++ Next_v1/src/components/home/ContactUs.tsx
@@ -10,21 +10,32 @@ const ContactUs = () => {
         <div className="card-container">
           <div className="card">
             <p className="font-[tossface] text-2xl sm:text-3xl sm:mb-3">📞</p>
-            <h3 className="font-semibold text-lg sm:text-xl mb-5">Call</h3>
+            <h3 className="font-semibold text-lg sm:text-xl mb-2">Call</h3>
+            <br />
             <p className="font-medium text-base sm:text-lg">02-300-0637</p>
           </div>
           <div className="card">
             <p className="font-[tossface] text-2xl sm:text-3xl sm:mb-3">📧</p>
-            <h3 className="font-semibold text-lg sm:text-xl mb-5">E-Mail</h3>
-            <p className="font-medium text-base sm:text-lg">Prof: goldmunt@gmail.com</p>
-            <p className="font-medium text-base sm:text-lg">Lab: example@gmail.com</p>
+            <h3 className="font-semibold text-lg sm:text-xl mb-2">E-Mail</h3>
+            <br />
+            <div className="flex flex-col ">
+              <a href="mailto:goldmunt@gmail.com" target="_blank" className="font-medium text-base lg:text-lg sm:text-base">
+                Prof: goldmunt@gmail.com
+              </a>
+              <a href="mailto:example@gmail.com" target="_blank" className="font-medium text-base lg:text-lg sm:text-base">
+                Lab: example@gmail.com
+              </a>
+            </div>
           </div>
           <div className="card">
             <p className="font-[tossface] text-2xl sm:text-3xl sm:mb-3">🏠</p>
-            <h3 className="font-semibold text-lg sm:text-xl mb-5">Address</h3>
+            <h3 className="font-semibold text-lg sm:text-xl mb-2">Address</h3>
+            <br />
             <p className="font-medium text-base sm:text-lg">명지대학교 인문캠퍼스</p>
-            <p className="font-medium text-base sm:text-lg">Prof : S1822</p>
-            <p className="font-medium text-base sm:text-lg">LAB : S21010</p>
+            <div className="flex flex-col ">
+              <p className="font-medium text-base sm:text-lg">Prof : S1822</p>
+              <p className="font-medium text-base sm:text-lg">LAB : S21010</p>
+            </div>
           </div>
         </div>
       </div>
