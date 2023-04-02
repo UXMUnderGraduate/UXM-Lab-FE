b402179c226fffb3ec6550c7d9ecf00b0482d4a0
MinsuKim <minsu020302@gmail.com>
2023-04-02 16:07:55 +0900

    Fix : companyAndDepartment type 추가

--- Next_v1/src/utils/fillter.ts
+++ Next_v1/src/utils/fillter.ts
@@ -8,6 +8,7 @@ interface Member {
   email: string;
   degree: string;
   img?: string;
+  companyAndDepartment?: string;
 }
 
 export function filteredList(dummy: Member[], degreeFilter: string[], isAlumniFilter: boolean): Member[] {
