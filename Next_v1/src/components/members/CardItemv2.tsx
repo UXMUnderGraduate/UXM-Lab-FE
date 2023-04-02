cf9b9d32337e28d6cc87cfc4abd65147cfa3577b
MinsuKim <minsu020302@gmail.com>
2023-04-02 16:02:05 +0900

    Feat : 회사, 연구분야 추가

--- Next_v1/src/components/members/CardItemv2.tsx
+++ Next_v1/src/components/members/CardItemv2.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 
 interface CardItemv2Props {
   id?: number;
-  img: string;
+  img?: string;
   nameKr: string;
   nameEn?: string;
   email?: string;
@@ -12,12 +12,14 @@ interface CardItemv2Props {
   isAlumni?: boolean;
   researchField?: string;
   degree?: string;
+  else?: string;
+  companyAndDepartment?: string;
 }
 
 const CardItemv2: FC<CardItemv2Props> = (props) => {
   return (
     <div className="sm:flex p-5 md:pl-14 text-left py-6">
-      <Image src={props.img} alt={props.nameKr} width={1300} height={1300} className="w-full  sm:w-52 sm:h-full" />
+      {props.img && <Image src={props.img} alt={props.nameKr} width={500} height={500} className="w-full  sm:w-52 sm:h-full" />}
       <div className="sm:pl-5 sm:pt-0 pt-3">
         <h1 className="font-black text-xl sm:text-3xl">{props.nameKr + '(' + props.nameEn + ')'}</h1>
         <h2 className="font-bold sm:text-2xl">{props.position}</h2>
@@ -28,6 +30,24 @@ const CardItemv2: FC<CardItemv2Props> = (props) => {
             <h4 className=" font-medium sm:text-xl">{props.email}</h4>
           </span>
         </div>
+        {props.researchField && (
+          <div className="flex">
+            <span className="font-bold sm:text-xl">researchField : &nbsp;</span>
+            <span>
+              <h4 className=" font-medium sm:text-xl">{props.researchField}</h4>
+            </span>
+          </div>
+        )}
+        {props.companyAndDepartment && (
+          <div className="flex">
+            <span className="font-bold sm:text-xl">Company : &nbsp;</span>
+            <span>
+              <h4 className=" font-medium sm:text-xl">{props.companyAndDepartment}</h4>
+            </span>
+          </div>
+        )}
+        {props.else && <h1 className="font-bold text-xl mt-3 sm:text-2xl">Biography</h1>}
+        {props.else && <p className="sm:text-2xl sm:mb-10">{props.else}</p>}
       </div>
     </div>
   );
