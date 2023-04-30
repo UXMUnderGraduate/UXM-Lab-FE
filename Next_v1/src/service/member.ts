709c8a00bc6ba8b742af54c74d1ea2553a914368
MinsuKim <minsu020302@gmail.com>
2023-05-01 03:43:13 +0900

    Feat : member데이터 fetching 완성

---
+++ Next_v1/src/service/member.ts
@@ -0,0 +1,20 @@
+import { userType } from '@/types';
+import axios from 'axios';
+
+export async function getMembers(): Promise<userType[]> {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_PATH}/members`);
+  const data = response.data;
+  console.log('전체 멤버 요청중');
+  // handle data here, such as filtering or formatting
+  return data;
+}
+
+export async function getMember(id: string): Promise<userType> {
+  const response = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_PATH}/members/${id}`);
+  const data = response.data;
+  console.log(`${id}의 멤버 요청중`);
+  // handle data here, such as filtering or formatting
+  return data;
+}
+
+
