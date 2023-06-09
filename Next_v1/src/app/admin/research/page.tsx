0c043647a8bf12403186a6ad4eb4d4fdef504593
MinsuKim <minsu020302@gmail.com>
2023-06-09 18:27:01 +0900

    master

--- Next_v1/src/app/admin/research/page.tsx
+++ Next_v1/src/app/admin/research/page.tsx
@@ -1,11 +1,19 @@
+import DetailResearchCard from '@/components/admin/DetailResearchCard';
+import GridContainer from '@/components/admin/GridContainer';
 import Layout from '@/components/admin/Layout';
+import { getResearchAll } from '@/service/research';
 import React from 'react';
 
-export default function page() {
+export default async function page() {
+  const research = await getResearchAll();
   return (
     <div className="mt-20 w-full">
       <Layout backPath="/admin">
-        <p>test</p>
+        <GridContainer>
+          {research.map((item) => (
+            <DetailResearchCard key={item.id} {...item}></DetailResearchCard>
+          ))}
+        </GridContainer>
       </Layout>
     </div>
   );
