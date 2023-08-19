c566eeb0376fee90c6038925e18ab8f379edce15
MinsuKim <minsu020302@gmail.com>
2023-08-19 14:39:37 +0900

    Chore : 오타 수정

--- Next_v1/src/app/previous_work/page.tsx
+++ Next_v1/src/app/previous_work/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ImgList from '@/components/gallery/ImgList';
 import Heading from '@/components/ui/Heading';
 
-export default function Gallery() {
+export default function PreviousWork() {
   return (
     <>
-      <Heading>Gallery</Heading>
+      <Heading>PreviousWork</Heading>
       <article className="gallery-article">
         <ImgList />
       </article>
